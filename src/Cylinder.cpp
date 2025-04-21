321cec3efc4911e66ddd797efe196b2527377435
Chihchi77 <jemmyfan0421@gmail.com>
2025-04-21 20:20:00 +0800

    Create OOP_Lab7

--- src/Cylinder.cpp
+++ src/Cylinder.cpp
@@ -2,32 +2,41 @@
 # define CYLINDER_CPP
 
 # include "Cylinder.h"
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+
+double pi = M_PI;
 
 double Cylinder :: SurfaceArea(){
-    
-    return ;
+    return pi * 2.0 * radius * radius + pi * 2.0 * radius * height;
 }
 
 double Cylinder :: Volume(){
-
-    return ;
+    return pi * radius * radius * height;
 }
 
 double Cylinder :: Circumference(){
-
-    return ;
+    return pi * radius * 2.0;
 }
 
 istream & operator>>(istream & in, Cylinder & cldr)
 {
-
-    return ;
+    in >> cldr.radius >> cldr.height;
+    return in;
 }
 
 ostream & operator<<(ostream & out, Cylinder & cldr)
 {
-    
-    return ;
+    double C = cldr.Circumference();
+    double A = cldr.SurfaceArea();
+    double V = cldr.Volume();
+    out << "Circumference: " << fixed << setprecision(3) << C << endl;
+    out << "SurfaceArea: " << fixed << setprecision(3) << A << endl;
+    out << "Volume: " << fixed << setprecision(3) << V << endl;
+    return out;
 }
 
 # endif
